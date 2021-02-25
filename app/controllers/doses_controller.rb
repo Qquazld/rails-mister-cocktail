176b37c8c0df2ab1822d2fcaddaaa2dfbc61be47
yann letertre <yannletertre10@gmail.com>
2021-02-25 17:08:29 +0100

    sauvegarde mister_cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,17 @@
 class DosesController < ApplicationController
+
+
+    def create
+        @dose = Dose.new(dose_params)
+        @cocktail = Cocktail.find(params[:cocktail_id])
+        @dose.cocktail = @cocktail
+        @dose.save
+        redirect_to @cocktail
+    end
+
+    private
+
+    def dose_params
+        params.require(:dose).permit(:description, :ingredient_id)
+    end
 end
